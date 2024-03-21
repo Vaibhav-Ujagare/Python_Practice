cde22980b596a5736171c470adc0afe8057e92c2
Vaibhav Ujagare <139372531+Vaibhav-Ujagare@users.noreply.github.com>
2024-03-21 06:40:20 +0000

    CompleteLinkedList

--- Data Structure/LinkedLint.py
+++ Data Structure/LinkedLint.py
@@ -1,26 +1,104 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self,data=None,next=None):
         self.data = data
-        self.next = None
-        self.secnext = None
+        self.next = next
+        self.head = None
 
+    def insert_at_begning(self,data):
+        node = Node(data,self.head)
+        self.head=node
 
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node4 = Node(40)
-node5 = Node(50)
+    def print(self):
+        itr = self.head
+        llstr = ''
+        while itr:
+            suffix = ""
+            if itr.next:
+                suffix = "-->"
+            llstr+=str(itr.data)+suffix
+            itr=itr.next
+        print(llstr)
 
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-new_node = Node(60)
-# head = node1
-current = node1
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        itr = self.head
+        while itr.next:
+            itr=itr.next
+        itr.next = Node(data)
 
-while current is not None:
-    print(current.data,end="-->")
-    current=current.next
-# current.next=new_node
-print("None")
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+        return count
+
+    def insert_at(self,index,data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+        elif index == 0:
+            self.insert_at_begning(data)
+        else:
+            itr = self.head
+            count = 0
+            while itr:
+                if count == index -1:
+                    node = Node(data,itr.next)
+                    itr.next = node
+                    break
+                itr = itr.next
+                count+=1
+
+    def remove_at(self,index):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+        elif index == 0:
+            self.head = self.head.next
+        else:
+            itr = self.head
+            count = 0
+            while itr:
+                if count == index - 1:
+                    itr.next = itr.next.next
+                    break
+                itr = itr.next
+                count+=1
+    
+    def insert_bulk_values(self,data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def insert_after_value(self,data_after,data_to_insert):
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                node = Node(data_to_insert,itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+
+    def remove_by_value(self,data):
+        itr = self.head
+        llstr=''
+        while itr: 
+            suffix=""
+            if itr.next:
+                suffix="-->"   
+            if itr.data != data:                 
+                llstr+=str(itr.data)+suffix          
+                
+            itr = itr.next
+        print(llstr)
+
+            
+        
+
+if __name__ == '__main__':
+    root = Node()
+    root.insert_bulk_values(["a","b","v"])
+    root.print()
+    root.remove_by_value("a")
